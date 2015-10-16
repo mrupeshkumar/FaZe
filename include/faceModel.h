73c58606ceb47be389af19085854e12ecbe67f94
mrupeshkumar <dmrupeshkumar@gmail.com>
2015-10-16 16:15:58 +0530

    minor changes to facemodel

--- include/faceModel.h
+++ include/faceModel.h
@@ -23,44 +23,44 @@ private:
 		1 : INDEX_RIGHT_EYE_PUPIL
 	*/
 
-	void computePupil(int mode);
-	void computeNormal();
-	void computeGaze(int mode);
-    std::vector<cv::Point> getIntermediateDescriptors(int index);
+		void computePupil(int mode);
+		void computeNormal();
+		void computeGaze(int mode);
+		std::vector<cv::Point> getIntermediateDescriptors(int index);
 
-public:
-	static const int MODE_PUPIL_SP = 0;
-	static const int MODE_PUPIL_CDF = 1;
+	public:
+		static const int MODE_PUPIL_SP = 0;
+		static const int MODE_PUPIL_CDF = 1;
 
-	static const int MODE_GAZE_VA = 0;
-	static const int MODE_GAZE_QE = 1;
+		static const int MODE_GAZE_VA = 0;
+		static const int MODE_GAZE_QE = 1;
 
-	static const int INDEX_LEFT_EYE = 0;
-	static const int INDEX_LEFT_EYEBROW = 1;
-	static const int INDEX_RIGHT_EYE = 2;
-	static const int INDEX_RIGHT_EYEBROW = 3;
-	static const int INDEX_NOSE_UPPER = 4;
-	static const int INDEX_NOSE_LOWER = 5;	
-	static const int INDEX_MOUTH_OUTER = 6;
-	static const int INDEX_MOUTH_INNER = 7;
-	static const int INDEX_LEFT_EYE_PUPIL = 8;
-	static const int INDEX_RIGHT_EYE_PUPIL = 9;
+		static const int INDEX_LEFT_EYE = 0;
+		static const int INDEX_LEFT_EYEBROW = 1;
+		static const int INDEX_RIGHT_EYE = 2;
+		static const int INDEX_RIGHT_EYEBROW = 3;
+		static const int INDEX_NOSE_UPPER = 4;
+		static const int INDEX_NOSE_LOWER = 5;	
+		static const int INDEX_MOUTH_OUTER = 6;
+		static const int INDEX_MOUTH_INNER = 7;
+		static const int INDEX_LEFT_EYE_PUPIL = 8;
+		static const int INDEX_RIGHT_EYE_PUPIL = 9;
 
-	static const int ORIGIN_IMAGE = 0;
-	static const int ORIGIN_FACE_CENTRE = 1;
+		static const int ORIGIN_IMAGE = 0;
+		static const int ORIGIN_FACE_CENTRE = 1;
 
-	static const int DESCRIPTOR_GLOBAL = 0;
-	static const int DESCRIPTOR_LOCAL = 1;
+		static const int DESCRIPTOR_GLOBAL = 0;
+		static const int DESCRIPTOR_LOCAL = 1;
 
-    void assign(full_object_detection shape, cv::Mat image, int modePupil, int modeGaze);
+		void assign(full_object_detection shape, cv::Mat image, int modePupil, int modeGaze);
 
-    cv::Point getPupil(int mode);
-    std::vector<cv::Point> getDescriptors(int index, int mode);
-    std::vector<double> getGaze();
-    std::vector<double> getNormal();
+		cv::Point getPupil(int mode);
+		std::vector<cv::Point> getDescriptors(int index, int mode);
+		std::vector<double> getGaze();
+		std::vector<double> getNormal();
 
-    void setOrigin(cv::Point origin);
-    void setOrigin(int mode);
-};
+		void setOrigin(cv::Point origin);
+		void setOrigin(int mode);
+	};
 
 #endif
